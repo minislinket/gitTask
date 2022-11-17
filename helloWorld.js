1e161c395f0f4d36494354ef0bfb7b4d3410b693
Ciska <ciskavdmerwe@gmail.com>
2022-11-17 15:58:10 +0200

    modified helloWorld.js from an alert to a prompt box

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,7 @@
-function showAlert() {
-    alert ("Git is Awesome!");
-  }
+let person = prompt("Please enter your name", "Harry Potter");
+let text;
+if (person == null || person == "") {
+  text = "User cancelled the prompt.";
+} else {
+  text = "Hello " + person + "! How are you today?";
+}
